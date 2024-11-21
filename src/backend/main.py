0983a129f934cfdae8064a9fb4d03929cd0a9b6f
USSC\aterentev <aleksandr.terentev@udv.group>
2024-11-21 22:29:40 +0500

    Добавил supports_credentials

--- src/backend/main.py
+++ src/backend/main.py
@@ -5,6 +5,8 @@ from controllers.main_page_controller import main_page_blueprint
 
 if __name__ == '__main__':
     app = Flask(__name__)
-    cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+    cors = CORS(app,
+                resources={r"/api/*": {"origins": "*"}},
+                supports_credentials=True)
     app.register_blueprint(main_page_blueprint)
     app.run(debug=True)
