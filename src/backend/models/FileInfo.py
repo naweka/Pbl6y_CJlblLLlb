9d446c4f323371ac3aceab00e8a7308debc1b91e
naweka <dredd88888888@gmail.com>
2024-12-07 18:14:38 +0500

    Фикс получения файла из карточки

--- src/backend/models/FileInfo.py
+++ src/backend/models/FileInfo.py
@@ -6,5 +6,5 @@ class FileInfo:
     id:str
     name:str
     alias_name:str
-    spectrogram_file:Optional[bytes]
-    is_sounds_detected:Optional[bool]
+    # spectrogram_file:Optional[bytes]
+    # is_sounds_detected:Optional[bool]
