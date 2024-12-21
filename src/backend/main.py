4a929f091b737bf5de1a8bce44127ad17479a4ea
naweka <dredd88888888@gmail.com>
2024-12-21 18:42:49 +0500

    Обновил CORS

--- src/backend/main.py
+++ src/backend/main.py
@@ -7,8 +7,9 @@ from services.MlService import ml_event_loop
 if __name__ == '__main__':
     app = Flask(__name__)
     cors = CORS(app,
-                resources={r"*": {"origins": "*"}},
-                supports_credentials=True)
+                resources={r'*': {'origins': '*'}},
+                supports_credentials=True,
+                expose_headers=['Content-Disposition'])
     app.register_blueprint(main_page_blueprint)
     
     ml_event_loop_thread = Thread(target=ml_event_loop, daemon=True, name='ml_event_loop')
