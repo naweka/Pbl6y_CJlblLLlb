1879f62478efe668525e7e70ed9edcec28cde39b
Coreytaz <coreytaz321@gmail.com>
2024-12-30 02:26:30 +0500

    feat: :sparkles: add locustfile test

--- src/backend/locustfile.py
+++ src/backend/locustfile.py
@@ -1,23 +1,48 @@
 from locust import HttpUser, TaskSet, task, between
 import json
 
-class UserBehavior(TaskSet):
 
+class UserBehavior(TaskSet):
     @task
     def get_cards(self):
-        headers = {'Content-Type': 'application/json'}
-        payload = {
-            'search_text': '',
-            'tags': ['qwerty']
+        headers = {
+            "Content-Type": "application/json",
+            "authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMDEwMTAxMDEtMDgwOC0wODA4LTA4MDgtMDgwODA4MDgwODA4IiwiZXhwIjoxNzM1NTI4Mjk3fQ.wgErDp-sONXAAO8R9flrlphQPsId5P3Xbyoc1bpUPE8",
         }
+        payload = {"search_text": "", "tags": ["qwerty"]}
         self.client.post("/api/v1/getCards", data=json.dumps(payload), headers=headers)
 
+
+class TagsBehavior(TaskSet):
+    @task
+    def get_tags(self):
+        headers = {
+            "Content-Type": "application/json",
+            "authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMDEwMTAxMDEtMDgwOC0wODA4LTA4MDgtMDgwODA4MDgwODA4IiwiZXhwIjoxNzM1NTI4Mjk3fQ.wgErDp-sONXAAO8R9flrlphQPsId5P3Xbyoc1bpUPE8",
+        }
+        self.client.get("/api/v1/getTags", headers=headers)
+
+
+class GenerateGuidBehavior(TaskSet):
+    @task
+    def get_generateGuid(self):
+        headers = {
+            "Content-Type": "application/json",
+            "authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMDEwMTAxMDEtMDgwOC0wODA4LTA4MDgtMDgwODA4MDgwODA4IiwiZXhwIjoxNzM1NTI4Mjk3fQ.wgErDp-sONXAAO8R9flrlphQPsId5P3Xbyoc1bpUPE8",
+        }
+        self.client.get(
+            "/api/v1/generateGuid", headers=headers
+        )
+
+
 class WebsiteUser(HttpUser):
-    tasks = [UserBehavior]
+    tasks = [UserBehavior, TagsBehavior, GenerateGuidBehavior]
     wait_time = between(1, 5)  # Wait time between tasks
+
 
 if __name__ == "__main__":
     import os
+
     os.system("locust -f locustfile.py")
-#locust -f locustfile.py
-#http://localhost:8089
+# locust -f locustfile.py
+# http://localhost:8089
